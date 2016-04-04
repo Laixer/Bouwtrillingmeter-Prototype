7bcacf56d2adc2d056bd57770ddb36b7ff9cb9a1
yorickdewid <yorick17@outlook.com>
2016-04-04 23:03:04 +0200

    Initial commit

---
+++ app/src/main/java/com/gemeenterotterdam/bouwtrillingsmeter/Location.java
@@ -0,0 +1,78 @@
+package com.gemeenterotterdam.bouwtrillingsmeter;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Location extends AppCompatActivity {
+
+    TextView textview;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        MeasurementLocationListener gpsTracker = new MeasurementLocationListener(this);
+        if (gpsTracker.getIsGPSTrackingEnabled())
+        {
+            String stringLatitude = String.valueOf(gpsTracker.latitude);
+            textview = (TextView)findViewById(R.id.field_latitude);
+            textview.setText(stringLatitude);
+
+            String stringLongitude = String.valueOf(gpsTracker.longitude);
+            textview = (TextView)findViewById(R.id.field_longitude);
+            textview.setText(stringLongitude);
+
+            String country = gpsTracker.getCountryName(this);
+            textview = (TextView)findViewById(R.id.field_country);
+            textview.setText(country);
+
+            String city = gpsTracker.getLocality(this);
+            textview = (TextView)findViewById(R.id.field_city);
+            textview.setText(city);
+
+            String postalCode = gpsTracker.getPostalCode(this);
+            textview = (TextView)findViewById(R.id.field_postal_code);
+            textview.setText(postalCode);
+
+            String addressLine = gpsTracker.getAddressLine(this);
+            textview = (TextView)findViewById(R.id.field_address_line);
+            textview.setText(addressLine);
+
+            if (gpsTracker.latitude != 0 && gpsTracker.longitude != 0) {
+                button = (Button) findViewById(R.id.btn_confirm);
+                button.setEnabled(true);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Location.this.finish();
+                        // TODO Auto-generated method stub
+                        //Intent myIntent = new Intent(MainActivity.this, Location.class);
+                        //MainActivity.this.startActivity(myIntent);
+                        ///
+                        //Intent intent = new Intent(this, MyActivity.class);
+                        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+                        //startActivity(intent);
+                        //this.finish();
+                    }
+                });
+            }
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+}
